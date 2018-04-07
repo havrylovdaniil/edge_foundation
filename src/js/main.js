107a41f2c8a5e761b1ff4ae9df3310e62eadf115
Havrylov Daniil <dp070593gda@gmail.com>
2018-04-07 10:08:29 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/* JQuery */
+//= ../../bower_components/jquery/dist/jquery.js
+
+/* Zurb Foundation */
+//= ../../bower_components/foundation-sites/dist/js/foundation.min.js
+
+
+//= ../../bower_components/foundation-sites/js/foundation.responsiveMenu.js
